61270a624f2f63f7da90584df0b5ebf2f9af1657
ericvyc <eric_vilar2007@hotmail.com>
2018-07-08 14:48:27 -0400

    Proxy configuration and Spring boot integration.

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Task} from "../tasks.model";
+import {TaskService} from "../task.service";
 
 @Component({
   selector: 'app-tasks-list',
@@ -10,14 +11,15 @@ export class TasksListComponent implements OnInit {
 
     tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit() {
-
-      this.tasks.push(new Task(1,"Task 1", true, "07/06/18"));
-      this.tasks.push(new Task(2,"Task 2", false, "10/06/18"));
-      this.tasks.push(new Task(3,"Task 3", false, "12/06/18"));
-
+      return this.taskService.getTasks().subscribe(
+          (tasks: any[]) => {
+              this.tasks = tasks;
+          },
+          (error) => console.log(error)
+      );
   }
 
   getDueDateLabel(task: Task) {
